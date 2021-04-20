0ac3f603b61197b3aae62d3bc92d4d17e153f24f
idnyaneshwaraxioned <dnyaneshwari@prdxn.com>
2021-04-20 11:04:39 +0530

    footer btn js done

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,17 @@ $(document).ready(function(){
 		$('.menu-right').toggleClass('active');
 		$('.menu-left').toggleClass('active');
     })
+
+    $('.banner-slider').slick({
+		autoplay: false,
+		autoplaySpeed: 2000,
+    });
+    
+    $(".footer-btn").click(function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
 });
-
-
-
 
 
 
